83d7afd40f748174a3d52ef95b9d4f7d81ad7034
Grant Luna <grant.luna@fnf.com>
2022-06-09 13:32:54 -0700

    Add color and black buttons to change the color of the boxes when hovered.

--- javascript.js
+++ javascript.js
@@ -7,30 +7,20 @@ const defaultGrid = () => {
 };
 defaultGrid();
 
-// A variable to get a random number between 0 and 255 for rgb
-const rgb = () => {
-  let number = Math.floor(Math.random() * 256);
-  return number;
-};
-
-// A function to change the color of the sketch to black
-let box = Array.from(document.querySelectorAll(".box"));
+// Changing the box backgrounds when hovered over
+let box = document.querySelectorAll(".box");
 box.forEach((box) => {
-  // box.addEventListener("mouseover", () => {
-  //   box.classList.add("black-background");
-  // });
   box.addEventListener("mouseover", () => {
-    box.style["background-color"] = `rgb(${rgb()}, ${rgb()}, ${rgb()})`;
+    box.classList.add("black-background");
   });
 });
+
 // Allowing the user to change the size of the box
-const button = document.querySelector(".user-choice");
-
+const boxSize = document.querySelector(".user-choice");
 let userChoice;
-let newBoxes;
 
-button.addEventListener("click", () => {
-  // Gather the user choice
+boxSize.addEventListener("click", () => {
+  // Gather the user choice & make sure its between 1 and 30
   userChoice = prompt(
     "How many dimensions do you want on each side of the box?"
   );
@@ -47,16 +37,36 @@ button.addEventListener("click", () => {
   container.style["grid-template-columns"] = `repeat(${userChoice}, 1fr)`;
   container.style["grid-template-rows"] = `repeat(${userChoice}, 1fr)`;
 
-  //
-
-  const newGrid = () => {
-    container.innerHTML = "";
-    for (let i = 1; i < userChoice * userChoice + 1; i++) {
-      container.innerHTML += `<div class="box box-${i}"</div>`;
-    }
-  };
   newGrid();
   box = document.querySelectorAll(".box");
+  box.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      box.style["background-color"] = "black";
+    });
+  });
+});
+
+// Clearing the sketch
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+  box.forEach((box) => {
+    box.style["background-color"] = "white";
+  });
+});
+
+// Change squares to black
+const black = document.querySelector(".black");
+black.addEventListener("click", () => {
+  box.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+      box.style["background-color"] = "black";
+    });
+  });
+});
+
+// Change squares to color
+const color = document.querySelector(".color");
+color.addEventListener("click", () => {
   box.forEach((box) => {
     box.addEventListener("mouseover", () => {
       box.style["background-color"] = `rgb(${rgb()}, ${rgb()}, ${rgb()})`;
@@ -64,10 +74,16 @@ button.addEventListener("click", () => {
   });
 });
 
-// A function to clear the sketch
-const clear = document.querySelector(".clear");
-clear.addEventListener("click", () => {
-  box.forEach((box) => {
-    box.style["background-color"] = "white";
-  });
-});
+// A function to create new divs dependent on the user input
+const newGrid = () => {
+  container.innerHTML = "";
+  for (let i = 1; i < userChoice * userChoice + 1; i++) {
+    container.innerHTML += `<div class="box box-${i}"</div>`;
+  }
+};
+
+// A variable to get a random number between 0 and 255 for rgb
+const rgb = () => {
+  let number = Math.floor(Math.random() * 256);
+  return number;
+};
